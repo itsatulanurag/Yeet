a22010df3b2cc18b5cc4cf199c2e153bfa7c919d
Atul <itsatulanurag@gmail.com>
2021-10-15 20:15:30 +0530

    .

---
+++ 69.sqrt-x.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=69 lang=cpp
+ *
+ * [69] Sqrt(x)
+ */
+
+// @lc code=start
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int mySqrt(int x)
+    {
+    }
+};
+// @lc code=end
