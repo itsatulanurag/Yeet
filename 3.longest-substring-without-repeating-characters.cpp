adbe4fb6a71ca3bfe45c1fe01d0472dcfb996d32
Atul <itsatulanurag@gmail.com>
2021-10-27 04:12:40 +0530

    ll

--- 3.longest-substring-without-repeating-characters.cpp
+++ 3.longest-substring-without-repeating-characters.cpp
@@ -5,11 +5,28 @@
  */
 
 // @lc code=start
-class Solution {
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
 public:
-    int lengthOfLongestSubstring(string s) {
-        
+    int lengthOfLongestSubstring(string s)
+    {
+        int left = 0, right = 0;
+        int ans = 0;
+        int chars[256] = {0};
+        while (right < s.length())
+        {
+            chars[s[right]]++;
+            while (chars[s[right]] > 1)
+            {
+                chars[s[left]]--;
+                left++;
+            }
+            ans = max(ans, right - left + 1);
+            right++;
+        }
+        return ans;
     }
 };
 // @lc code=end
-
