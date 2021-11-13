3239a510fa6c89a8c8b20cb9b3dbf502ed7518f2
Atul <itsatulanurag@gmail.com>
2021-11-14 05:24:51 +0530

    .

--- 121.best-time-to-buy-and-sell-stock.cpp
+++ 121.best-time-to-buy-and-sell-stock.cpp
@@ -13,6 +13,7 @@ class Solution
 public:
     int maxProfit(vector<int> &prices)
     {
+        //Kadence Algorithm
         int profit = 0;
         int mini = prices[0];
         for (int i = 1; i < prices.size(); i++)
