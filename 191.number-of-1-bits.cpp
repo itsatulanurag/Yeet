f1f4e590ad2716d15d159479958415189570a0f9
Atul <itsatulanurag@gmail.com>
2022-01-13 16:57:48 +0530

    easy-191

---
+++ 191.number-of-1-bits.cpp
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=191 lang=cpp
+ *
+ * [191] Number of 1 Bits
+ */
+
+// @lc code=start
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int hammingWeight(uint32_t n)
+    {
+        int res = 0;
+        for (int i = 0; i < 32; i++)
+            if ((n >> i) & 1)
+                res++;
+        return res;
+    }
+};
+// @lc code=end
